d24543976b1bff5fadbae05bdf7af9073ecaed63
German Valencia Vargas <german.valencia@kinandcarta.com>
2024-02-27 16:00:22 -0500

    Update UserService.java

--- webflux-example/src/main/java/com/example/webflux/UserService.java
+++ webflux-example/src/main/java/com/example/webflux/UserService.java
@@ -1,18 +1,9 @@
 package com.example.webflux;
 
-import com.sun.source.tree.BinaryTree;
-import org.javatuples.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.TreeSet;
-import java.util.stream.Collectors;
 
 @Service
 public class UserService {
